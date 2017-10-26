1eda18331e03da4a440e27e83e607a995499289b
Stephen Legge <stephen@aleggeup.com>
2017-10-26 01:58:57 -0400

    Make the apple spin

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,7 +6,17 @@ import { Component } from "@angular/core";
     <ActionBar title="Hello World!" class="action-bar"></ActionBar>
     <!-- Your UI components go here -->
     <Image src="~/images/apple.jpg"></Image>
-  `
+  `,
+  styles: [`
+    @keyframes spin {
+      from { transform: rotate(0); } to { transform: rotate(359); }
+    }
+    Image {
+      animation-name: spin; animation-duration: 3s;
+      animation-iteration-count: infinite;
+      animation-timing-function: linear;
+    }
+  `]
 })
 export class AppComponent {
   // Your TypeScript logic goes here
