5128a37379b060fb3d1a8b9c25c49ec3eacb54e9
Stephen Legge <stephen@aleggeup.com>
2017-10-26 01:15:01 -0400

    Experimental angular with nativescript Hello World project

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  template: `
+    <ActionBar title="Hello World!" class="action-bar"></ActionBar>
+    <!-- Your UI components go here -->
+  `
+})
+export class AppComponent {
+  // Your TypeScript logic goes here
+}
